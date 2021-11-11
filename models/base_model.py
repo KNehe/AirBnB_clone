97409260ce80f38fbdaaf27a02fc6ff3b27e0254
Nehemiah Kamolu <kamolunehemiah@gmail.com>
2021-11-11 18:35:46 +0000

    task 5 - store first object

--- models/base_model.py
+++ models/base_model.py
@@ -27,6 +27,7 @@ class BaseModel:
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             self.id = str(uuid4())
+            models.storage.new(self)
 
     def __str__(self):
         """Returns a string representation of the base model"""
@@ -38,6 +39,7 @@ class BaseModel:
         """Updates the attribute updated_at with the current datetime"""
 
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """"
